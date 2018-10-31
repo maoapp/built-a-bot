517fb2f4cc64db8bdf9a5c3aa8ce68f9677045c9
maoapp <maoapp@hotmail.com>
2018-10-31 16:54:45 -0500

    split store by modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import robotsModule from './modules/robots';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    cart: [],
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter(item => item.head.onSale);
-    },
+  modules: {
+    robots: robotsModule,
   },
 });
---
+++ src/store/modules/robots.js
@@ -0,0 +1,15 @@
+export default {
+  state: {
+    cart: [],
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+  },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter(item => item.head.onSale);
+    },
+  },
+};
